741b642bd3db7b9f8a36249cd2f289f585695258
cfrs2005 <cfrs2005@yahoo.com>
2014-04-03 20:51:54 +0800

    github i am coming
    Change-Id: I3c0024a81357ec660f82692def1d47e675b35a1b

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * index.php
+ * ==============================================
+ * Copy right 2013-2014 http://www.80aj.com
+ * ----------------------------------------------
+ * This is not a free software, without any authorization is not allowed to use and spread.
+ * ==============================================
+ * @param unknowtype
+ * @return return_type
+ * @author: 80aj
+ * @date: 2014-4-3
+ * @version: v1.0.0
+ */
+ 
+echo "hello word github,i am coming";
